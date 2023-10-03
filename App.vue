4c8a8b5b4e0db6590a0a7535b1ca3bf2eaa0fcb4
BladePawn <111053591+BladePawn@users.noreply.github.com>
2023-10-04 01:02:50 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script setup>
+import {computed, onMounted, reactive, ref} from "vue";
+
+onMounted(() => {
+  if (typeof cef != "undefined") {
+    cef.emit("game:hud:setComponentVisible", "interface", false);
+
+  }
+})
+
+</script>
+
+<style lang="scss">
+</style>
